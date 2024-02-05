b5a5154eb92fceb38c4650b862bfe702f768160f
Justyna <jus.sobcz@gmail.com>
2024-02-06 00:25:00 +0100

    feat: add patch, delete endpoints for places; modify getPlaceByUserId function

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,7 +1,7 @@
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const HttpError = require("../modules/http-error");
 
-const DUMMY_PLACES = [{
+let DUMMY_PLACES = [{
     id: 'p1',
     title: "Empire State Building",
     description: "One of the most famous sky scrapers in the world!",
@@ -24,15 +24,15 @@ const getPlaceById = (req, res, next) => {
     res.json({place})
 }
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
     const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(place => place.creator === userId);
+    const places = DUMMY_PLACES.filter(place => place.creator === userId);
 
-    if (!place) {
-        return next(new HttpError("Could not find a place for the provided user id.", 404));
+    if (!places || places.length === 0) {
+        return next(new HttpError("Could not find places for the provided user id.", 404));
     }
 
-    res.json({place});
+    res.json({places});
 }
 
 const createPlace = (req, res, next) => {
@@ -52,6 +52,28 @@ const createPlace = (req, res, next) => {
     res.status(201).json({place: createdPlace});
 }
 
+const updatePlace = (req, res, next) => {
+    const {title, description} = req.body;
+    const placeId = req.params.pid;
+
+    const updatePlace = {...DUMMY_PLACES.find(place => place.id === placeId)};
+    const placeIndex = DUMMY_PLACES.findIndex(place => place.id === placeId);
+    updatePlace.title = title;
+    updatePlace.description = description;
+
+    DUMMY_PLACES[placeIndex] = updatePlace;
+
+    res.status(200).json({place: updatePlace});
+
+}
+const deletePlace = (req, res, next) => {
+    const placeId = req.params.pid;
+    DUMMY_PLACES = DUMMY_PLACES.filter(place=>place.id !== placeId);
+    res.status(200).json({message: "Deleted place."})
+}
+
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
