c6528e87a22205ae9faa34d39cc740ec193826b2
Justyna <jus.sobcz@gmail.com>
2024-02-06 15:30:53 +0100

    feat: fetch geolocation coordinates from Google during the creation of the new place

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,6 +1,7 @@
 const {v4: uuidv4} = require('uuid');
-const {validationResult} = require("express-validator")
+const {validationResult} = require("express-validator");
 const HttpError = require("../modules/http-error");
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [{
     id: 'p1',
@@ -36,15 +37,22 @@ const getPlacesByUserId = (req, res, next) => {
     res.json({places});
 }
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        console.log(errors)
-        throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    if (!errors.isEmpty()) {
+        return next(new HttpError('Invalid inputs passed, please check your data.', 422));
     }
 
-    const {title, description, coordinates, address, creator} = req.body;
+    const {title, description, address, creator} = req.body;
+
+    let coordinates;
+    try {
+        coordinates = await getCoordsForAddress(address);
+    } catch (error) {
+        return next(error);
+    }
+
 
     const createdPlace = {
         id: uuidv4(),
@@ -63,7 +71,7 @@ const createPlace = (req, res, next) => {
 const updatePlace = (req, res, next) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         console.log(errors)
         throw new HttpError('Invalid inputs passed, please check your data.', 422);
     }
@@ -83,7 +91,7 @@ const updatePlace = (req, res, next) => {
 }
 const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
-    if(!DUMMY_PLACES.find(place=>place.id === placeId)){
+    if (!DUMMY_PLACES.find(place => place.id === placeId)) {
         throw new HttpError('Could not find a place for that id.', 404)
     }
 
---
+++ util/location.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+require('dotenv').config();
+const HttpError = require('../modules/http-error');
+
+const API_KEY = process.env.API_LOCATION_KEY;
+
+const getCoordsForAddress = async (address) => {
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`)
+    const data = response.data;
+
+    if (!data || data.status === 'ZERO_RESULTS') {
+        throw new HttpError('Could not find location for the specified address.', 422);
+    }
+
+    return data.results[0].geometry.location;
+}
+
+module.exports = getCoordsForAddress;
