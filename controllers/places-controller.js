b7ed1848b56f917fe0338710cfbec35adcd2d90d
Justyna <jus.sobcz@gmail.com>
2024-02-08 02:47:27 +0100

    feat: refactor getPlaceByUserId method, by using populate

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -39,20 +39,20 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
     const userId = req.params.uid;
-    let places;
+    let userWithPlaces;
 
     try {
-        places = await Place.find({creator: userId});
+        userWithPlaces = await User.findById(userId).populate('places');
     } catch (err) {
         const error = new HttpError('Fetching places failed, please try again later.', 500);
         return next(error);
     }
 
-    if (!places || places.length === 0) {
+    if (!userWithPlaces || userWithPlaces.length === 0) {
         return next(new HttpError("Could not find places for the provided user id.", 404));
     }
 
-    res.json({places: places.map(place => place.toObject({getters: true}))});
+    res.json({places: userWithPlaces.places.map(place => place.toObject({getters: true}))});
 }
 
 const createPlace = async (req, res, next) => {
@@ -143,6 +143,7 @@ const updatePlace = async (req, res, next) => {
     res.status(200).json({place: place.toObject({getters: true})});
 
 }
+
 const deletePlace = async (req, res, next) => {
     const placeId = req.params.pid;
     let place;
