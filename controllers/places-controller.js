412a574cecfdd32ceecc4109c48a49ea7640fa25
Justyna <jus.sobcz@gmail.com>
2024-02-12 22:28:22 +0100

    feat: add authorization to the update and delete place endpoints

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -119,6 +119,11 @@ const updatePlace = async (req, res, next) => {
         return next(error);
     }
 
+    if (place.creator.toString() !== req.userData.userId) {
+        const error = new HttpError('You are not allowed to edit this place.', 401);
+        return next(error);
+    }
+
     place.title = title;
     place.description = description;
 
@@ -146,6 +151,11 @@ const deletePlace = async (req, res, next) => {
 
     if (!place) {
         const error = new HttpError('Could not find a place for this id.', 404);
+        return next(error);
+    }
+
+    if (place.creator.id !== req.userData.userId) {
+        const error = new HttpError('You are not allowed to delete this place.', 401);
         return next(error);
     }
 
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -103,7 +103,7 @@ const login = async (req, res, next) => {
     }
 
     if (!existingUser) {
-        const error = new HttpError('Invalid credentials, could not log you in.', 401);
+        const error = new HttpError('Invalid credentials, could not log you in.', 403);
         return next(error);
     }
 
@@ -116,7 +116,7 @@ const login = async (req, res, next) => {
     }
 
     if (!isValidPassword) {
-        const error = new HttpError('Invalid credentials, could not log you in.', 401);
+        const error = new HttpError('Invalid credentials, could not log you in.', 403);
         return next(error);
     }
 
